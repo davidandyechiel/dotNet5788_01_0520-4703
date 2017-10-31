866580c5e1cb2a163a78308fe56c79a078389e3d
yechiel chabany <Yechel@users.noreply.github.com>
2017-10-31 17:44:10 +0200

    testing

---
+++ dotNet5788_01_0520-4703/dotNet5788_01_0520-4703/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5788_01_0520_4703
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(    "Hello_World");
+        }
+    }
+}

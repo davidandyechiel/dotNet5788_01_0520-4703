6d8868f7b056189e09f760de72782ef261136443
David Salem <dsalem9191@gmail.com>
2017-11-01 18:14:39 +0200

    david

--- dotNet5788_01_0520-4703/dotNet5788_01_0520-4703/Program.cs
+++ dotNet5788_01_0520-4703/dotNet5788_01_0520-4703/Program.cs
@@ -12,7 +12,7 @@ namespace dotNet5788_01_0520_4703
         {
             Console.WriteLine(    "Hello_World");
             Console.WriteLine("another testing");
-            Console.WriteLine("changing in yechiel");
+            Console.WriteLine("changing in david");
         }
     }
 }

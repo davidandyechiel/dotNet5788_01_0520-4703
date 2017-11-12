51a68739fda541fdcf5687b69d7f4f6e6f6d3a29
yechiel chabany <Yechel@users.noreply.github.com>
2017-11-12 12:17:48 +0200

    nothing

--- dotNet5788_01_0520-4703/dotNet5788_01_0520-4703/Program.cs
+++ dotNet5788_01_0520-4703/dotNet5788_01_0520-4703/Program.cs
@@ -4,15 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace dotNet5788_01_0520_4703
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-            Console.WriteLine("another testing");
-            Console.WriteLine("changing in yechiel");
+            pickAnum();
+        }
+
+
+
+        static void pickAnum()
+        {
+            Console.WriteLine("Please choose question");
+            switch ((Console.Read()))
+            {
+                case 1:
+                    { }
+                    break;
+                case 2:
+
+                    break;
+                 defulte;
+                    pickAnum();
+            }
         }
     }
 }
--- dotNet5788_01_0520-4703/dotNet5788_01_0520-4703/question1.cs
+++ dotNet5788_01_0520-4703/dotNet5788_01_0520-4703/question1.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace dotNet5788_01_0520_4703
-{
-    class question1
-    {
-    }
-}
+﻿
